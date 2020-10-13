3365c10b9817c6e5ce7e1c3e970fda721d9da6d2
aura6601 <55701863+aura6601@users.noreply.github.com>
2020-10-14 00:23:49 +0300

    Add files via upload

--- v.03.cpp
+++ v.03.cpp
@@ -1,26 +1,7 @@
 #include "funkcijos.h"
-
-struct duomenys
-{
-    string vardai;
-    string pavardes;
-    vector<string> pazymiai;
-};
-
-struct studentas
-{
-    string vardai;
-    string pavardes;
-    vector<int> pazymiai;
-};
-
-struct rusiavimas
-{
-    string vardai;
-    string pavardes;
-    double vidurkiai;
-    double medianos;
-};
+#include "duomenys.h"
+#include "studentas.h"
+#include "rusiavimas.h"
 
 bool palygintiVardus(rusiavimas& a, rusiavimas& b) { return a.vardai < b.vardai; };
 bool palygintiPavardes(rusiavimas& a, rusiavimas& b) { return a.pavardes < b.pavardes; };
